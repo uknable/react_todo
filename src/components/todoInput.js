4de6ce8cb939bcbf57e0f6af1d6ddb2d6482e8c8
uknable <uknable@hotmail.com>
2017-07-13 11:11:36 +1000

    list styles

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 function TodoInput(props) {
   return (
-    <form onSubmit={props.onSubmit}>
-      <input type="text"
-             value={props.taskInput}
-             onChange={props.onChange}/>
+    <div className="todo-input-container">
+      <form onSubmit={props.onSubmit}>
+        <input type="text"
+               value={props.taskInput}
+               onChange={props.onChange}/>
 
-      <input type="submit"
-             value="Add"
-             className='btn btn-default'/>
-    </form>
+        <input type="submit"
+               value="Add"
+               className='btn btn-default'/>
+      </form>
+    </div>
   );
 };
 
